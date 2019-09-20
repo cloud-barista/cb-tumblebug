4272adcb526c2b685c2c724ed48ea639ba5aaa76
Jihoon Seo <jihoon.seo@etri.re.kr>
2019-09-20 18:31:50 +0900

    Implement image mgmt functions

--- mcism_master/mcism_master.go
+++ mcism_master/mcism_master.go
@@ -141,6 +141,48 @@ func apiServer() {
 	e.DELETE("/mcis/:id", restDelMcis)
 	e.DELETE("/mcis", restDelAllMcis)
 
+	e.POST("/resources/image", restPostImage)
+	e.GET("/resources/image/:cbImageId", restGetImage)
+	e.GET("/resources/image", restGetAllImage)
+	e.PUT("/resources/image/:cbImageId", restPutImage)
+	e.DELETE("/resources/image/:cbImageId", restDelImage)
+	e.DELETE("/resources/image", restDelAllImage)
+	/*
+		e.POST("/resources/spec", restPostSpec)
+		e.GET("/resources/spec/:id", restGetSpec)
+		e.GET("/resources/spec", restGetAllSpec)
+		e.PUT("/resources/spec/:id", restPutSpec)
+		e.DELETE("/resources/spec/:id", restDelSpec)
+		e.DELETE("/resources/spec", restDelAllSpec)
+
+		e.POST("/resources/network", restPostNetwork)
+		e.GET("/resources/network/:id", restGetNetwork)
+		e.GET("/resources/network", restGetAllNetwork)
+		e.PUT("/resources/network/:id", restPutNetwork)
+		e.DELETE("/resources/network/:id", restDelNetwork)
+		e.DELETE("/resources/network", restDelAllNetwork)
+
+		e.POST("/resources/subnet", restPostSubnet)
+		e.GET("/resources/subnet/:id", restGetSubnet)
+		e.GET("/resources/subnet", restGetAllSubnet)
+		e.PUT("/resources/subnet/:id", restPutSubnet)
+		e.DELETE("/resources/subnet/:id", restDelSubnet)
+		e.DELETE("/resources/subnet", restDelAllSubnet)
+
+		e.POST("/resources/securityGroup", restPostSecurityGroup)
+		e.GET("/resources/securityGroup/:id", restGetSecurityGroup)
+		e.GET("/resources/securityGroup", restGetAllSecurityGroup)
+		e.PUT("/resources/securityGroup/:id", restPutSecurityGroup)
+		e.DELETE("/resources/securityGroup/:id", restDelSecurityGroup)
+		e.DELETE("/resources/securityGroup", restDelAllSecurityGroup)
+
+		e.POST("/resources/sshKey", restPostSshKey)
+		e.GET("/resources/sshKey/:id", restGetSshKey)
+		e.GET("/resources/sshKey", restGetAllSshKey)
+		e.PUT("/resources/sshKey/:id", restPutSshKey)
+		e.DELETE("/resources/sshKey/:id", restDelSshKey)
+		e.DELETE("/resources/sshKey", restDelAllSshKey)
+	*/
 	e.Logger.Fatal(e.Start(":1323"))
 
 }
@@ -158,6 +200,14 @@ func main() {
 	fmt.Println("[Terminate MCIS]:\t curl <ServerIP>:1323/mcis/<McisID>?action=terminate")
 	fmt.Println("[Del MCIS Info]:\t curl -X DELETE <ServerIP>:1323/mcis/<McisID>")
 	fmt.Println("[Del MCISs Info]:\t curl -X DELETE <ServerIP>:1323/mcis")
+
+	fmt.Println("\n")
+	fmt.Println("[List Images]:\t\t curl <ServerIP>:1323/image")
+	fmt.Println("[Create Image]:\t\t curl -X POST <ServerIP>:1323/image?action=create -H 'Content-Type: application/json' -d '{<IMAGE_REQ_JSON>}'")
+	fmt.Println("[Register Image]:\t\t curl -X POST <ServerIP>:1323/image?action=register -H 'Content-Type: application/json' -d '{<IMAGE_REQ_JSON>}'")
+	fmt.Println("[Get Image Info]:\t curl <ServerIP>:1323/image/<imageID>")
+	fmt.Println("[Del Image Info]:\t curl -X DELETE <ServerIP>:1323/image/<imageID>")
+	fmt.Println("[Del Images Info]:\t curl -X DELETE <ServerIP>:1323/image")
 
 	// load config
 	masterConfigInfos = confighandler.GetMasterConfigInfos()
