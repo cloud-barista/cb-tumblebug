7bbb24d4a5ed7ada14676f2df060c4565d90c6b5
Jihoon Seo <jihoon.seo@etri.re.kr>
2019-11-05 21:19:24 +0900

    Add publicIp tester

---
+++ tester-sh/publicip/spider-get-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ../setup.env
+
+for NAME in "${CONNECT_NAMES[@]}"
+do
+	ID=`curl -sX GET http://$RESTSERVER:1024/publicip?connection_name=${NAME} |json_pp |grep "\"Name\" :" |awk '{print $3}' | head -n 1 |sed 's/"//g' |sed 's/,//g'`
+	curl -sX GET http://$RESTSERVER:1024/publicip/${ID}?connection_name=${NAME} |json_pp &
+done
