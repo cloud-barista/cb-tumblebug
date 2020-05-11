3f20a6781261e2361f7bf1f3f5c4433197760413
seokho-son <shsongist@gmail.com>
2020-05-11 11:00:33 +0900

    Add test script for NameSpace

---
+++ test/2020-04-29/1.vNet/test-vNet.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ../conf.env
+
+echo "####################################################################"
+echo "## 1. VPC: Create"
+echo "####################################################################"
+
+INDEX=${1}
+
+curl -sX POST http://localhost:1323/tumblebug/ns/$NS_ID/resources/vNet -H 'Content-Type: application/json' -d \
+	'{
+		"name": "VPC-0'$INDEX'",
+		"connectionName": "'${CONN_CONFIG[INDEX]}'",
+		"cidrBlock": "192.168.0.0/16",
+		"subnetReqInfoList": [ {
+			"Name": "Subnet-0'$INDEX'", 
+			"IPv4_CIDR": "192.168.1.0/24"
+		} ]
+	}' | json_pp #|| return 1
