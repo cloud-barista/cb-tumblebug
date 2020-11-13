c8aefe772f4be5e44f4a44ed8e2447091efa85fe
Seokho Son <shsongist@gmail.com>
2020-11-13 18:07:58 +0900

    Update runLoadMaker.sh

--- assets/scripts/runLoadMaker.sh
+++ assets/scripts/runLoadMaker.sh
@@ -6,7 +6,7 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update > /dev/null
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io > /dev/null
-sudo docker run -d -p 8080:80 -p 4433:443 jihoons/docker-nginx-systemcall:latest
+sudo docker run -d -p 8888:80 -p 4433:443 jihoons/docker-nginx-systemcall:latest
 
 # Print a message (IP address)
 str=$(curl https://api.ipify.org)
