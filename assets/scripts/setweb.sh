9e823d529501a8aca35e284a2face245cf2f75db
Seokho Son <shsongist@gmail.com>
2020-11-21 21:31:00 +0900

    Update setweb.sh

--- assets/scripts/setweb.sh
+++ assets/scripts/setweb.sh
@@ -14,7 +14,7 @@ sudo sed -i "s/Thank you for using nginx/Thank you for using Cloud-Barista and C
 
 sudo sed -i "s/<a href=\"http:\/\/nginx.com\/\">nginx.com<\/a>.<\/p>/<a href=\"https:\/\/tools.keycdn.com\/geo?host=`curl https://api.ipify.org`\">Check the Location of NGINX HOST<\/a>.<\/p>/g" /var/www/html/index.nginx-debian.html
 
-sudo sed -i "s/:\/\/nginx.org\/\">nginx.org/s:\/\/`curl https://api.ipify.org`:8888\/\">Call to generate CPU load/g" /var/www/html/index.nginx-debian.html
+sudo sed -i "s/:\/\/nginx.org\/\">nginx.org/:\/\/`curl https://api.ipify.org`:8888\/\">. Call to generate CPU load/g" /var/www/html/index.nginx-debian.html
 
 str=$(curl https://api.ipify.org)
 
