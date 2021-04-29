300d4b613e12895321692928518a3492238c4713
seokho-son <shsongist@gmail.com>
2021-04-29 22:37:23 +0900

    Fix message in Korean

--- src/core/common/namespace_test.go
+++ src/core/common/namespace_test.go
@@ -10,7 +10,7 @@ func TestNs(t *testing.T) {
 	/*
 		    expected := 1
 		    actual := 0
-			assert.Equal(t, expected, actual, "기대값과 결과값이 다릅니다.")
+			assert.Equal(t, expected, actual, "not approriate output.")
 	*/
 
 	nsName := "tb-unit-test"
@@ -19,17 +19,17 @@ func TestNs(t *testing.T) {
 	nsReq.Name = nsName
 
 	result, _ := CreateNs(&nsReq)
-	assert.Equal(t, nsName, result.Name, "CreateNs 기대값과 결과값이 다릅니다.")
+	assert.Equal(t, nsName, result.Name, "CreateNs: not approriate output.")
 
 	result, _ = GetNs(nsName)
-	assert.Equal(t, nsName, result.Name, "GetNs 기대값과 결과값이 다릅니다.")
+	assert.Equal(t, nsName, result.Name, "GetNs: not approriate output.")
 
 	//result, _ := ListNs()
 
 	//result, _ := ListNsId()
 
 	resultBool, _ := CheckNs(nsName)
-	assert.Equal(t, true, resultBool, "CheckNs 기대값과 결과값이 다릅니다.")
+	assert.Equal(t, true, resultBool, "CheckNs: not approriate output.")
 
 	resultErr := DelNs(nsName)
 	assert.Nil(t, resultErr)
