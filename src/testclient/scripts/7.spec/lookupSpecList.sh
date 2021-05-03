bfd76a79c78bbae0f68cceb64e3a1237f6b16b16
seokho-son <shsongist@gmail.com>
2021-05-03 16:40:34 +0900

    Provide elapsed time for spec tests

--- src/testclient/scripts/7.spec/lookupSpecList.sh
+++ src/testclient/scripts/7.spec/lookupSpecList.sh
@@ -2,37 +2,40 @@
 
 #function lookup_spec_list() {
 
+SECONDS=0
 
-	TestSetFile=${4:-../testSet.env}
-    
-    FILE=$TestSetFile
-    if [ ! -f "$FILE" ]; then
-        echo "$FILE does not exist."
-        exit
-    fi
-	source $TestSetFile
-    source ../conf.env
-	AUTH="Authorization: Basic $(echo -n $ApiUsername:$ApiPassword | base64)"
+TestSetFile=${4:-../testSet.env}
 
-	echo "####################################################################"
-	echo "## 7. spec: Lookup Spec List"
-	echo "####################################################################"
+FILE=$TestSetFile
+if [ ! -f "$FILE" ]; then
+	echo "$FILE does not exist."
+	exit
+fi
+source $TestSetFile
+source ../conf.env
+AUTH="Authorization: Basic $(echo -n $ApiUsername:$ApiPassword | base64)"
 
-	CSP=${1}
-	REGION=${2:-1}
-	POSTFIX=${3:-developer}
+echo "####################################################################"
+echo "## 7. spec: Lookup Spec List"
+echo "####################################################################"
 
-	source ../common-functions.sh
-	getCloudIndex $CSP
+CSP=${1}
+REGION=${2:-1}
+POSTFIX=${3:-developer}
 
-	resp=$(
-        curl -H "${AUTH}" -sX GET http://$TumblebugServer/tumblebug/lookupSpecs -H 'Content-Type: application/json' -d @- <<EOF
+source ../common-functions.sh
+getCloudIndex $CSP
+
+resp=$(
+	curl -H "${AUTH}" -sX GET http://$TumblebugServer/tumblebug/lookupSpecs -H 'Content-Type: application/json' -d @- <<EOF
 		{ 
-			"connectionName": "${CONN_CONFIG[$INDEX,$REGION]}"
+			"connectionName": "${CONN_CONFIG[$INDEX, $REGION]}"
 		}
 EOF
-    ); echo ${resp} | jq ''
-    echo ""
-#}
+)
+echo ${resp} | jq ''
+echo ""
+
+printElapsed $@
 
 #lookup_spec_list
