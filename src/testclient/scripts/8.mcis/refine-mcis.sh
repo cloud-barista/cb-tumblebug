f5e1a58eed97c908ac24d89cf946de1f7ba42161
seokho-son <shsongist@gmail.com>
2021-05-30 00:35:18 +0900

    Add script for refine mcis

---
+++ src/testclient/scripts/8.mcis/refine-mcis.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+TestSetFile=${4:-../testSet.env}
+if [ ! -f "$TestSetFile" ]; then
+	echo "$TestSetFile does not exist."
+	exit
+fi
+source $TestSetFile
+source ../conf.env
+
+echo "####################################################################"
+echo "## 8. VM: Refine MCIS (remove failed VMs)"
+echo "####################################################################"
+
+CSP=${1}
+REGION=${2:-1}
+POSTFIX=${3:-developer}
+
+source ../common-functions.sh
+getCloudIndex $CSP
+
+
+if [ "${INDEX}" == "0" ]; then
+	MCISID=${MCISPREFIX}-${POSTFIX}
+else
+	MCISID=${CONN_CONFIG[$INDEX,$REGION]}-${POSTFIX}
+fi
+
+echo "${MCISID}"
+
+ControlCmd=refine
+curl -H "${AUTH}" -sX GET http://$TumblebugServer/tumblebug/ns/$NSID/mcis/${MCISID}?action=${ControlCmd} | jq ''
