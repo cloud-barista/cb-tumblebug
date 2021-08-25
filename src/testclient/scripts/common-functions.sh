f6fcd0dd03f5e7f28fbebf5ddf727aa4a40bcb30
seokho-son <shsongist@gmail.com>
2021-08-25 17:02:12 +0900

    Add named parameter options

--- src/testclient/scripts/common-functions.sh
+++ src/testclient/scripts/common-functions.sh
@@ -10,23 +10,41 @@ function dozing() {
 	echo "(Finish dozing. Back to work)"
 }
 
+function readParameters() {
+	CSP=${1}
+	REGION=${2:-1}
+	POSTFIX=${3:-dev01}
+	TestSetFile=${4:-../testSet.env}
+}
+
 function readParametersByName() {
+
 	CSP="all"
 	REGION="1"
-	POSTFIX="user"
+	POSTFIX="dev01"
 	TestSetFile="../testSet.env"
 	OPTION01=""
 	OPTION02=""
 	OPTION03=""
 
+	FlagNamedPram=""
+
+	echo ""
 	# Update values for network parameters by named input parameters (i, c)
-	while getopts ":f:n:x:y:z:h:" opt; do
+	while getopts ":n:f:c:r:x:y:z:h:" opt; do
+		FlagNamedPram="yes"
 		case $opt in
+		n)
+			POSTFIX="$OPTARG"
+			;;
 		f)
 			TestSetFile="$OPTARG"
 			;;
-		n)
-			POSTFIX="$OPTARG"
+		c)
+			CSP="$OPTARG"
+			;;
+		r)
+			REGION="$OPTARG"
 			;;
 		x)
 			OPTION01="$OPTARG"
@@ -38,34 +56,62 @@ function readParametersByName() {
 			OPTION03="$OPTARG"
 			;;
 		h)
-			echo "How to use '-h' (ex: ./${0##*/} -c ../testSet.env -n myname)"
+			echo "[Usage] ./${0##*/} -param value -param value"
+			echo "[Example] ./${0##*/} -n myname01 -f ../testSet.env"
+			echo ""
+			echo " -n [postfix of resources to generate and retrieve] (ex: -n myname01, default: $POSTFIX)"
+			echo " -f [file path to describe a cloud test set] (ex: -f ../testSet01.env, default: $TestSetFile)"
+			echo ""
+			echo " -c [specific cloud type] (ex: -c aws, optional)"
+			echo " -r [index of a specific zone of the cloud type] (ex: -r 3, optional)"
+			echo ""
+			echo " -x [any value passed to a parameter of the command] (ex: -x vmid01, optional)"
+			echo " -y [any value passed to a parameter of the command] (ex: -y 3, optional)"
+			echo " -z [any value passed to a parameter of the command] (ex: -z file, optional)"
 			exit 0
 			;;
 		\?)
-			echo "Invalid option -$OPTARG (Use: -i for NETWORK_INTERFACE, -c for POD_NETWORK_CIDR)" >&2
+			echo "[Warrning] Invalid option [$@]. -$OPTARG is not an option" >&2
+			echo ""
+			echo "[Usage] ./${0##*/} -param value -param value"
+			echo "[Example] ./${0##*/} -n myname01 -f ../testSet.env"
+			echo ""
+			echo " -n [postfix of resources to generate and retrieve] (ex: -n myname01, default: $POSTFIX)"
+			echo " -f [file path to describe a cloud test set] (ex: -f ../testSet01.env, default: $TestSetFile)"
+			echo ""
+			echo " -c [specific cloud type] (ex: -c aws, optional)"
+			echo " -r [index of a specific zone of the cloud type] (ex: -r 3, optional)"
+			echo ""
+			echo " -x [any value passed to a parameter of the command] (ex: -x vmid01, optional)"
+			echo " -y [any value passed to a parameter of the command] (ex: -y 3, optional)"
+			echo " -z [any value passed to a parameter of the command] (ex: -z file, optional)"
 			exit 0
 			;;
 		esac
 	done
 
+	if [ -z "$FlagNamedPram" ]; then
+			echo "[Warrning] Invalid option [$@]"
+			echo ""
+			echo "[Usage] ./${0##*/} -param value -param value"
+			echo "[Example] ./${0##*/} -n myname01 -f ../testSet.env"
+			echo ""
+			echo " -n [postfix of resources to generate and retrieve] (ex: -n myname01, default: $POSTFIX)"
+			echo " -f [file path to describe a cloud test set] (ex: -f ../testSet01.env, default: $TestSetFile)"
+			echo ""
+			echo " -c [specific cloud type] (ex: -c aws, optional)"
+			echo " -r [index of a specific zone of the cloud type] (ex: -r 3, optional)"
+			echo ""
+			echo " -x [any value passed to a parameter of the command] (ex: -x vmid01, optional)"
+			echo " -y [any value passed to a parameter of the command] (ex: -y 3, optional)"
+			echo " -z [any value passed to a parameter of the command] (ex: -z file, optional)"
+		exit 0
+	fi
+
 	echo "[Input parameters]"
-	echo "CSP: $CSP"
-	echo "REGION: $REGION"
-	echo "POSTFIX: $POSTFIX"
-	echo "OPTION01: $OPTION01"
-	echo "OPTION02: $OPTION02"
-	echo "OPTION03: $OPTION03"
+	echo "[POSTFIX: $POSTFIX] | [TestSetFile: $TestSetFile] | [CSP: $CSP] | [REGION: $REGION]"
+	echo "[OPTION01: $OPTION01] | [OPTION02: $OPTION02] | [OPTION03: $OPTION03]"
 
-
-}
-
-function readParameters() {
-	CSP=${1}
-	REGION=${2:-1}
-	POSTFIX=${3:-developer}
-
-	echo "[Input parameters]"
-	echo "CSP: $CSP, REGION: $REGION, POSTFIX: $POSTFIX,"
 }
 
 function printElapsed() {
