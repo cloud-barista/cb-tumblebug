afbbaf193b2a1bd46b57e6e2d27b358f69502bfa
Seokho Son <shsongist@gmail.com>
2021-09-14 15:07:11 +0900

    Add user input to confirm test config (#734)

--- src/testclient/scripts/sequentialFullTest/check-test-config.sh
+++ src/testclient/scripts/sequentialFullTest/check-test-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "####################################################################"
-echo "## Check test config file (-n deveoperPrefix -f ../testSetCustom.env)"
+echo "## Check test config file (-n deveoperPrefix -f ../testSetCustom.env -x numOfVMsInEachVMGroup)"
 echo "####################################################################"
 
 source ../init.sh
@@ -10,28 +10,41 @@ NUMVM=${OPTION01:-1}
 
 echo ""
 echo "[Configuration in ($TestSetFile) & (../conf.env) files]"
-echo "1) TumblebugServer : $TumblebugServer // SpiderServer : $SpiderServer"
-echo "2) NameSpace ID : $NSID // MCIS ID : $MCISID"
+echo "1) Tumblebug Server : $TumblebugServer // Spider Server : $SpiderServer"
+
 
 INDEXX=${NumCSP}
-echo "3) Enabled CSPs for $MCISID"
+echo "2) Enabled CSPs and regions in $TestSetFile"
 for ((cspi = 1; cspi <= INDEXX; cspi++)); do
 	CSP=${CSPType[$cspi]}
 	INDEXY=${NumRegion[$cspi]}
-	echo "[$cspi] $CSP (enabled regions : $INDEXY)"
+	echo " - [$cspi] $CSP (enabled regions : $INDEXY)"
 done
 echo ""
+
 for ((cspi = 1; cspi <= INDEXX; cspi++)); do
 	INDEXY=${NumRegion[$cspi]}
 	CSP=${CSPType[$cspi]}
-	echo "[$cspi] $CSP details"
+	echo "   [$cspi] $CSP details"
 	for ((cspj = 1; cspj <= INDEXY; cspj++)); do
-		echo "[$cspi,$cspj] ${RegionName[$cspi,$cspj]}" 
-		echo "- VM SPEC : ${SPEC_NAME[$cspi,$cspj]}"
-		echo "- VM IMAGE : ${IMAGE_NAME[$cspi,$cspj]}"
+		echo "   [$cspi,$cspj] ${RegionName[$cspi,$cspj]}" 
+		echo "    - VM SPEC : ${SPEC_NAME[$cspi,$cspj]}"
+		echo "    - VM IMAGE : ${IMAGE_NAME[$cspi,$cspj]}"
 	done
 	echo ""
 done
 
+echo "3) MCIS Configuration"
+echo " - NameSpace ID : $NSID"
+echo " - MCIS ID : $MCISID"
+echo " - Number of VMs"
+
+for ((cspi = 1; cspi <= INDEXX; cspi++)); do
+	INDEXY=${NumRegion[$cspi]}
+	CSP=${CSPType[$cspi]}
+	TOTALVM=$((1 * INDEXY * NUMVM))
+	echo "   - [$cspi] VMs($TOTALVM) = $CSP(1) * Region($INDEXY) * VMgroup($NUMVM)"
+done
 
 echo ""
+
--- src/testclient/scripts/sequentialFullTest/create-all.sh
+++ src/testclient/scripts/sequentialFullTest/create-all.sh
@@ -2,6 +2,14 @@
 
 SECONDS=0
 
+./check-test-config.sh "$@"
+
+read -p 'Confirm the above configuration. Do you want to proceed ? (y/n) : ' CHECKPROCEED
+if [ "${CHECKPROCEED}" != "y" ]; then
+	echo "[Command: $0 $@] has been cancelled. See you soon. :)"
+    exit 0
+fi
+
 ./create-mcir-ns-cloud.sh "$@"
 
 ./create-mcis-only.sh "$@"
