8cd42437427dd7db97679a48b73ceafe457817db
seokho-son <shsongist@gmail.com>
2021-09-14 22:44:51 +0900

    Display output of MCIS status script in a table

--- src/testclient/scripts/8.mcis/status-mcis.sh
+++ src/testclient/scripts/8.mcis/status-mcis.sh
@@ -19,6 +19,13 @@ echo "${MCISID}"
 ControlCmd=status
 curl -H "${AUTH}" -sX GET http://$TumblebugServer/tumblebug/ns/$NSID/mcis/${MCISID}?action=${ControlCmd} | jq ''
 
+echo "Table: All VMs in the MCIS : ${MCISID}"
+echo ""
+curl -H "${AUTH}" -sX GET http://$TumblebugServer/tumblebug/ns/$NSID/mcis/${MCISID}?action=${ControlCmd} |
+    jq '.status | .vm' |
+    jq -r '(["ID","Status","PublicIP","PrivateIP","CloudType","CloudRegion","CreatedTime"] | (., map(length*"-"))), (.[] | [.id, .status, .publicIp, .privateIp, .location.cloudType, .location.nativeRegion, .createdTime]) | @tsv' |
+    column -t
+
 #HTTP_CODE=$(curl -o /dev/null -w "%{http_code}\n" -H "${AUTH}" "http://${TumblebugServer}/tumblebug/ns/$NSID/mcis/${MCISID}?action=status" --silent)
 #echo "Status: $HTTP_CODE"
 
