e39c225254455f4645efd836f1158420eca55b7a
Yunkon (Alvin) Kim <hermitkim1@users.noreply.github.com>
2021-09-14 16:10:10 +0900

    Apply recursive confirmation prompt (#735)
    - Add recursive confirmation prompt
    - Allow uppercase or lowercase letters (Y, y, N, n)

--- src/testclient/scripts/sequentialFullTest/create-all.sh
+++ src/testclient/scripts/sequentialFullTest/create-all.sh
@@ -4,11 +4,20 @@ SECONDS=0
 
 ./check-test-config.sh "$@"
 
-read -p 'Confirm the above configuration. Do you want to proceed ? (y/n) : ' CHECKPROCEED
-if [ "${CHECKPROCEED}" != "y" ]; then
-	echo "[Command: $0 $@] has been cancelled. See you soon. :)"
-    exit 0
-fi
+while true; do
+    read -p 'Confirm the above configuration. Do you want to proceed ? (y/n) : ' CHECKPROCEED
+    case $CHECKPROCEED in
+        [Yy]* ) 
+            break
+            ;;
+        [Nn]* ) 
+            echo "[Command: $0 $@] has been cancelled. See you soon. :)"
+            exit 1
+            ;;
+        * ) 
+            echo "Please answer yes or no.";;
+    esac
+done
 
 ./create-mcir-ns-cloud.sh "$@"
 
