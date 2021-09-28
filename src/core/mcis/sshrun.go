3a7f6e863dcd936fb53f7b90e3c97017378f2e24
seokho-son <shsongist@gmail.com>
2021-09-28 22:01:06 +0900

    Add license note

--- src/core/mcis/sshrun.go
+++ src/core/mcis/sshrun.go
@@ -1,11 +1,15 @@
-// Package for VM's SSH and SCP of CB-Spider.
-// The CB-Spider is a sub-Framework of the Cloud-Barista Multi-Cloud Project.
-// The CB-Spider Mission is to connect all the clouds with a single interface.
-//
-//      * Cloud-Barista: https://github.com/cloud-barista
-//
-// by CB-Spider Team, 2019.10.
-// Imported from CB-Spider
+/*
+Copyright 2019 The Cloud-Barista Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package mcis
 
