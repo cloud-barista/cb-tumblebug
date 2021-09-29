5b28a35ccef52d274fbbfa6b604751f2e6db280e
Seokho Son <shsongist@gmail.com>
2021-09-30 04:17:36 +0900

    Apply new description in test conf.env

--- src/testclient/scripts/sequentialFullTest/check-test-config.sh
+++ src/testclient/scripts/sequentialFullTest/check-test-config.sh
@@ -9,24 +9,24 @@ source ../init.sh
 NUMVM=${OPTION01:-1}
 #echo -e "${NC} ${GREEN} ${BOLD}"
 echo ""
-echo -e "[${BOLD}Configuration${NC} in ${GREEN} $TestSetFile${NC} & ${GREEN} ../conf.env ${NC} files]"
+echo -e "[${BOLD}Configuration${NC} in ${GREEN}${BOLD} $TestSetFile${NC} & ${GREEN}${BOLD} ../conf.env ${NC} files]"
 echo ""
 echo -e "${BOLD}1) System Endpoints${NC}"
-echo -e " - Tumblebug Server : ${GREEN} $TumblebugServer ${NC}"
-echo -e " - Spider Server : ${GREEN} $SpiderServer ${NC}"
+echo -e " - Tumblebug Server : ${GREEN}${BOLD} $TumblebugServer ${NC}"
+echo -e " - Spider Server : ${GREEN}${BOLD} $SpiderServer ${NC}"
 echo ""
 
 INDEXX=${NumCSP}
-echo -e "${BOLD}2) Enabled CSPs and regions${NC}"
+echo -e "${BOLD}2) Enabled Clouds and Regions${NC}"
 
 for ((cspi = 1; cspi <= INDEXX; cspi++)); do
 	INDEXY=${NumRegion[$cspi]}
 	CSP=${CSPType[$cspi]}
-	echo -e "${GREEN} - [$cspi] $CSP (enabled regions : $INDEXY)${NC}"
+	echo -e "${GREEN}${BOLD} - [$cspi] Cloud : $CSP (enabled regions : $INDEXY)${NC}"
 	for ((cspj = 1; cspj <= INDEXY; cspj++)); do
-		echo -e "${YELLOW}   [$cspi,$cspj] ${RegionName[$cspi,$cspj]}" 
+		echo -e "${YELLOW}   [$cspi,$cspj] Region : ${RegionLocation[$IX,$IY]} (${RegionName[$cspi,$cspj]})" 
 		echo -e "    - VM SPEC : ${SPEC_NAME[$cspi,$cspj]}"
-		echo -e "    - VM IMAGE : ${IMAGE_NAME[$cspi,$cspj]} ${NC}"
+		echo -e "    - VM IMAGE : ${IMAGE_TYPE[$cspi,$cspj]} (${IMAGE_NAME[$cspi,$cspj]}) ${NC}"
 	done
 	echo ""
 done
@@ -40,15 +40,15 @@ for ((cspi = 1; cspi <= INDEXX; cspi++)); do
 done
 
 echo -e "${BOLD}3) MCIS Configuration${NC}"
-echo -e " - NameSpace ID :${GREEN} $NSID${NC}"
-echo -e " - MCIS ID :${GREEN} $MCISID${NC}"
-echo -e " - Number of Total VMs :${GREEN} $TotalVM${NC}"
+echo -e " - NameSpace ID :${GREEN}${BOLD} $NSID${NC}"
+echo -e " - MCIS ID :${GREEN}${BOLD} $MCISID${NC}"
+echo -e " - Number of Total VMs :${GREEN}${BOLD} $TotalVM${NC}"
 
 for ((cspi = 1; cspi <= INDEXX; cspi++)); do
 	INDEXY=${NumRegion[$cspi]}
 	CSP=${CSPType[$cspi]}
 	RegionVM=$((1 * INDEXY * NUMVM))
-	echo -e "   - ${YELLOW}[$cspi] VMs($RegionVM) = $CSP(1) * Region($INDEXY) * Number Of VMs($NUMVM)${NC}"
+	echo -e "   - ${YELLOW}[$cspi] VMs($RegionVM) = $CSP(1) * Region($INDEXY) * Replication($NUMVM)${NC}"
 done
 
 echo ""
