c09a26f535a47f55aca933921eaddcbd9f436eef
chobobdev <chobobdev@naver.com>
2021-10-10 13:55:10 +0900

    [#745] Update clean-all.sh to ask for confirmation

--- src/testclient/scripts/sequentialFullTest/clean-all.sh
+++ src/testclient/scripts/sequentialFullTest/clean-all.sh
@@ -2,12 +2,36 @@
 
 SECONDS=0
 
++ source ../conf.env
++ source ../common-functions.sh
+
++ checkPrerequisite
+
++ ./check-test-config.sh "$@"
++ echo -e "${BOLD}"
++ while true; do
++     read -p 'Confirm the above configuration. Do you want to proceed to clean ? (y/n) : ' CHECKPROCEED
++     case $CHECKPROCEED in
++         [Yy]* ) 
++             break
++             ;;
++         [Nn]* ) 
++             echo
++             echo "Cancel [$0 $@]"
++             echo "See you soon. :)"
++             echo
++             exit 1
++             ;;
++         * ) 
++             echo "Please answer yes or no.";;
++     esac
++ done
+
 ./clean-mcis-only.sh "$@"
 
 ./clean-mcir-ns-cloud.sh "$@"
 
 duration=$SECONDS
 
-source ../common-functions.sh
 printElapsed $@
 echo "" >>./executionStatus.history
