a0c3b6b499b17fd4a125245f1ee67e0777a4057f
Seokho Son <shsongist@gmail.com>
2021-10-14 06:59:00 +0900

    common resource list .csv generator

---
+++ src/testclient/scripts/misc/gen-csv-image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "####################################################################"
+echo "## Gen csv file from script config"
+echo "####################################################################"
+
+source ../init.sh
+
+PRINT="ProviderName,connectionName,cspImageId,OsType"
+echo "${PRINT}"
+echo "${PRINT}" >./cloudimage.csv
+
+INDEXX=${NumCSP}
+for ((cspi = 1; cspi <= INDEXX; cspi++)); do
+	INDEXY=${NumRegion[$cspi]}
+	for ((cspj = 1; cspj <= INDEXY; cspj++)); do
+		PRINT="${ProviderName[$cspi]},${CONN_CONFIG[$cspi,$cspj]},${IMAGE_NAME[$cspi,$cspj]},${IMAGE_TYPE[$cspi,$cspj]}"
+		echo "$PRINT"
+		echo "$PRINT" >>./cloudimage.csv
+	done
+done
