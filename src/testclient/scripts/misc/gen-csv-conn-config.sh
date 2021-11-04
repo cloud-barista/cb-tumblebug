9ad69fba1be4856d63894356561a1c4a3c0adc46
seokho-son <shsongist@gmail.com>
2021-11-04 19:37:53 +0900

    Fix gen svc script to use total num CSP

--- src/testclient/scripts/misc/gen-csv-conn-config.sh
+++ src/testclient/scripts/misc/gen-csv-conn-config.sh
@@ -6,15 +6,15 @@ echo "####################################################################"
 
 source ../init.sh
 
-PRINT="ProviderName,CONN_CONFIG,RegionName,RegionLocation,DriverLibFileName,DriverName"
+PRINT="ProviderName,CONN_CONFIG,RegionName,NativeRegionName,RegionLocation,DriverLibFileName,DriverName"
 echo "${PRINT}"
 echo "${PRINT}" >./cloudconnection.csv
 
-INDEXX=${NumCSP}
+INDEXX=${TotalNumCSP}
 for ((cspi = 1; cspi <= INDEXX; cspi++)); do
-	INDEXY=${NumRegion[$cspi]}
+	INDEXY=${TotalNumRegion[$cspi]}
 	for ((cspj = 1; cspj <= INDEXY; cspj++)); do
-		PRINT="${ProviderName[$cspi]},${CONN_CONFIG[$cspi,$cspj]},${RegionName[$cspi,$cspj]},${RegionLocation[$cspi,$cspj]},${DriverLibFileName[$cspi]},${DriverName[$cspi]}"
+		PRINT="${ProviderName[$cspi]},${CONN_CONFIG[$cspi,$cspj]},${RegionName[$cspi,$cspj]},${RegionVal01[$cspi,$cspj]},${RegionLocation[$cspi,$cspj]},${DriverLibFileName[$cspi]},${DriverName[$cspi]}"
 		echo "$PRINT"
 		echo "$PRINT" >>./cloudconnection.csv
 	done
