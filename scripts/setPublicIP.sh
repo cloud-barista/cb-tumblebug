0d5562dc0b1638eb4a195b29635546f200139750
Seokho Son <shsongist@gmail.com>
2021-11-08 23:41:47 +0900

    Make script understand sourced

--- scripts/setPublicIP.sh
+++ scripts/setPublicIP.sh
@@ -7,6 +7,15 @@ echo ""
 echo "Setting SELF_ENDPOINT allows outside users access to CB-Tumblebug Swagger API Dashboard."
 echo ""
 
+if [[ "$(basename -- "$0")" == "setPublicIP.sh" ]]; then
+    echo
+    echo
+    echo "- Executing [$0] will not work properly"
+    echo "- Source the script with the following command [source $0]"
+    echo
+    exit 1
+fi
+
 # Get IP address which is accessable from outsite.
 # `https://api.ipify.org` is one of IP lookup services. If it is not available we need to change.
 str=$(curl https://api.ipify.org)
