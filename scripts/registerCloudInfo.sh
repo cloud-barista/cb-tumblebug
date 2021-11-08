e1ef70842479ced38c45c4ab921d93fe78cc78cb
seokho-son <shsongist@gmail.com>
2021-11-08 17:54:43 +0900

    Add oneclick initMC Env script

--- scripts/registerCloudInfo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "$CBTUMBLEBUG_ROOT" ]; then
-    SCRIPT_DIR=`dirname ${BASH_SOURCE[0]-$0}`
-    export CBTUMBLEBUG_ROOT=`cd $SCRIPT_DIR && cd .. && pwd`
-fi
-
-$CBTUMBLEBUG_ROOT/src/testclient/scripts/1.configureSpider/register-cloud-interactive.sh -n tb
