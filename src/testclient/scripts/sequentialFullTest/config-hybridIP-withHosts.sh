e58826c55733060a763a8a4fa737d500f85b5aa2
seokho-son <shsongist@gmail.com>
2021-11-12 13:45:45 +0900

    Expediate config-hybridIP script

--- src/testclient/scripts/sequentialFullTest/config-hybridIP-withHosts.sh
+++ src/testclient/scripts/sequentialFullTest/config-hybridIP-withHosts.sh
@@ -31,48 +31,51 @@ echo "[MCIS INFO: $MCISID]"
 # done
 
 for rowi in $(echo "${VMARRAY}" | jq -r '.[] | @base64'); do
-	_jq() {
-		echo ${rowi} | base64 --decode | jq -r ${1}
-	}
-
-	VMID=$(_jq '.id')
-	publicIP=$(_jq '.publicIP')
-	vNetId=$(_jq '.vNetId')
-
-	echo "VMID: $VMID"
-
-	VMKEYID=$(_jq '.sshKeyId')
-	KEYINFO=$(curl -H "${AUTH}" -sX GET http://$TumblebugServer/tumblebug/ns/${NSID}/resources/sshKey/${VMKEYID})
-	USERNAME=$(jq -r '.verifiedUsername' <<<"$KEYINFO")
-	KEYFILENAME="${VMKEYID}"
-
-	for rowj in $(echo "${VMARRAY}" | jq -r '.[] | @base64'); do
+	{
 		_jq() {
-			echo ${rowj} | base64 --decode | jq -r ${1}
+			echo ${rowi} | base64 --decode | jq -r ${1}
 		}
 
-		Bid=$(_jq '.id')
-		BpublicIP=$(_jq '.publicIP')
-		BprivateIP=$(_jq '.privateIP')
-		BvNetId=$(_jq '.vNetId')
+		VMID=$(_jq '.id')
+		publicIP=$(_jq '.publicIP')
+		vNetId=$(_jq '.vNetId')
 
-		CommandToAddHosts=""
+		echo "VMID: $VMID"
 
-		if [ "${VMID}" == "${Bid}" ]; then
-			CommandToAddHosts="sudo sed -i '2i127.0.0.1 ${Bid}' /etc/hosts"
-		elif [ "${vNetId}" == "${BvNetId}" ]; then
-			CommandToAddHosts="sudo sed -i '2i${BprivateIP} ${Bid}' /etc/hosts"
-		else
-			CommandToAddHosts="sudo sed -i '2i${BpublicIP} ${Bid}' /etc/hosts"
-		fi
+		VMKEYID=$(_jq '.sshKeyId')
+		KEYINFO=$(curl -H "${AUTH}" -sX GET http://$TumblebugServer/tumblebug/ns/${NSID}/resources/sshKey/${VMKEYID})
+		USERNAME=$(jq -r '.verifiedUsername' <<<"$KEYINFO")
+		KEYFILENAME="${VMKEYID}"
 
-		echo "${CommandToAddHosts}"
+		for rowj in $(echo "${VMARRAY}" | jq -r '.[] | @base64'); do
+			_jq() {
+				echo ${rowj} | base64 --decode | jq -r ${1}
+			}
 
-		VAR1=$(ssh -i ./sshkey-tmp/$KEYFILENAME.pem $USERNAME@$publicIP -o StrictHostKeyChecking=no "$CommandToAddHosts")
-		echo "${VAR1}" | jq ''
+			Bid=$(_jq '.id')
+			BpublicIP=$(_jq '.publicIP')
+			BprivateIP=$(_jq '.privateIP')
+			BvNetId=$(_jq '.vNetId')
 
-	done
+			CommandToAddHosts=""
+
+			if [ "${VMID}" == "${Bid}" ]; then
+				CommandToAddHosts="sudo sed -i '2i127.0.0.1 ${Bid}' /etc/hosts"
+			elif [ "${vNetId}" == "${BvNetId}" ]; then
+				CommandToAddHosts="sudo sed -i '2i${BprivateIP} ${Bid}' /etc/hosts"
+			else
+				CommandToAddHosts="sudo sed -i '2i${BpublicIP} ${Bid}' /etc/hosts"
+			fi
+
+			echo "${CommandToAddHosts}"
+
+			VAR1=$(ssh -i ./sshkey-tmp/$KEYFILENAME.pem $USERNAME@$publicIP -o StrictHostKeyChecking=no "$CommandToAddHosts")
+			echo "${VAR1}" | jq ''
+
+		done
+	} &
 done
+wait
 
 CMD="cat /etc/hosts"
 VAR1=$(
