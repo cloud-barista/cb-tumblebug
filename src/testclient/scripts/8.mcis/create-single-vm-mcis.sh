4e322959e08583ef4a89dfbcf63d98b1faf955fc
Jihoon Seo <jihoon.seo@etri.re.kr>
2021-11-15 18:25:48 +0900

    Add RootDiskType, RootDiskSize handling feature

--- src/testclient/scripts/8.mcis/create-single-vm-mcis.sh
+++ src/testclient/scripts/8.mcis/create-single-vm-mcis.sh
@@ -35,7 +35,9 @@
 				"vNetId": "'${CONN_CONFIG[$INDEX,$REGION]}'-'${POSTFIX}'",
 				"subnetId": "'${CONN_CONFIG[$INDEX,$REGION]}'-'${POSTFIX}'",
 				"description": "description",
-				"vmUserPassword": ""
+				"vmUserPassword": "",
+				"rootDiskType": "",
+				"rootDiskSize": "77"
 			}
 			]
 		}' | jq '' 
