fa3fdf4d234bdd07e1d0394195605bc45c401f86
seokho-son <shsongist@gmail.com>
2021-11-23 15:07:25 +0900

    Fix install agent script

--- src/testclient/scripts/9.monitoring/install-agent.sh
+++ src/testclient/scripts/9.monitoring/install-agent.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
 
-#function install_agent() {
+source ../init.sh
 
+echo "####################################################################"
+echo "## Install monitoring agent to MCIS "
+echo "####################################################################"
 
-	TestSetFile=${4:-../testSet.env}
-    if [ ! -f "$TestSetFile" ]; then
-        echo "$TestSetFile does not exist."
-        exit
-    fi
-	source $TestSetFile
-    source ../conf.env
-	
-	echo "####################################################################"
-	echo "## Install monitoring agent to MCIS "
-	echo "####################################################################"
-
-	CSP=${1}
-	REGION=${2:-1}
-	POSTFIX=${3:-developer}
-
-	source ../common-functions.sh
-	getCloudIndex $CSP
-
-	MCISID=${CONN_CONFIG[$INDEX,$REGION]}-${POSTFIX}
-
-	if [ "${INDEX}" == "0" ]; then
-		# MCISPREFIX=avengers
-		MCISID=${POSTFIX}
-	fi
-
-	curl -H "${AUTH}" -sX POST http://$TumblebugServer/tumblebug/ns/$NSID/monitoring/install/mcis/$MCISID -H 'Content-Type: application/json' -d \
-		'{
+curl -H "${AUTH}" -sX POST http://$TumblebugServer/tumblebug/ns/$NSID/monitoring/install/mcis/$MCISID -H 'Content-Type: application/json' -d \
+	'{
 			"command": "echo -n [CMD] Works! [Hostname: ; hostname ; echo -n ]"
-		}' | jq '' #|| return 1
-#}
-
-#install_agent
+	}' | jq ''
