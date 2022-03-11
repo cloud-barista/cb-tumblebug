b0344792fdbfdbb88e0154c1074cdfdaeef0dfe3
seokho-son <shsongist@gmail.com>
2022-03-11 11:25:37 +0900

    Add install Docker script

---
+++ scripts/installDocker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Need to be executed with sudo
+echo "[Install Docker]"
+
+echo
+echo "Will run the following command"
+echo "1) wget https://raw.githubusercontent.com/hermitkim1/scripts4Ubuntu/master/2.Docker-installation.sh"
+echo "2) source 2.Docker-installation.sh"
+
+wget https://raw.githubusercontent.com/hermitkim1/scripts4Ubuntu/master/2.Docker-installation.sh
+source 2.Docker-installation.sh
