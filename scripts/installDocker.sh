33db6bc0e506851b87840ba2d4b957ee181248e2
Seokho Son <shsongist@gmail.com>
2022-04-09 06:50:57 +0900

    Add Docker engine installation script (#1060)
    * Update runSpider container version
    
    * Update install docker script
    
    * Add install docker to mcis script

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -7,18 +7,19 @@ echo
 echo "Will run the following command (the script needs to be executed with sudo)"
 echo "1) sudo apt update -y"
 echo "2) wget -qO- get.docker.com | sh"
-echo "3) sudo docker info"
+echo "3) sudo docker -v"
 
-# Install Docker engine
-# update
-echo == apt update
-sudo apt update -y
+# Check Docker engine
+if ! sudo docker -v 2>&1; then
+    # update
+    echo "[apt update]"
+    sudo apt update -y
 
-echo == install Docker engine
-sleep 2
-# install docker
-wget -qO- get.docker.com | sh
+    # install Docker
+    echo "[install Docker engine]"
+    wget -qO- get.docker.com | sh
+fi
 
 # installation checking
-echo == sudo docker info
-sudo docker info
+echo [sudo docker -v]
+sudo docker -v
