63e63a8ce55d11903632fcb8ed5e9ed93722fd44
Seokho Son <shsongist@gmail.com>
2022-04-09 01:08:12 +0900

    Add install docker to mcis script

---
+++ src/testclient/scripts/sequentialFullTest/deploy-docker-runtime.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "####################################################################"
+echo "## Deploy (install) Docker to MCIS "
+echo "####################################################################"
+
+source ../init.sh
+
+CMD="wget https://raw.githubusercontent.com/cloud-barista/cb-tumblebug/main/scripts/installDocker.sh -O ~/installDocker.sh; chmod +x ~/installDocker.sh; sudo ~/installDocker.sh"
+echo "CMD: $CMD"
+
+VAR1=$(curl -H "${AUTH}" -sX POST http://$TumblebugServer/tumblebug/ns/$NSID/cmd/mcis/$MCISID -H 'Content-Type: application/json' -d @- <<EOF
+	{
+	"command"        : "${CMD}"
+	}
+EOF
+)
+echo "${VAR1}" | jq ''
+echo ""
