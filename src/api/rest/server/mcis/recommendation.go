ac8dc75409a0659378b0e04a94c39fd6ac75385d
seokho-son <shsongist@gmail.com>
2022-04-21 21:21:27 +0900

    Update recommend api path

--- src/api/rest/server/mcis/recommendation.go
+++ src/api/rest/server/mcis/recommendation.go
@@ -28,15 +28,14 @@ import (
 // @Tags [Infra service] MCIS Provisioning management
 // @Accept  json
 // @Produce  json
-// @Param nsId path string true "Namespace ID" default(common)
 // @Param deploymentPlan body mcis.DeploymentPlan false "Recommend MCIS plan (filter and priority)"
 // @Success 200 {object} []mcir.TbSpecInfo
 // @Failure 404 {object} common.SimpleMsg
 // @Failure 500 {object} common.SimpleMsg
-// @Router /ns/{nsId}/mcisRecommendVm [post]
+// @Router /mcisRecommendVm [post]
 func RestRecommendVm(c echo.Context) error {
 
-	nsId := c.Param("nsId")
+	nsId := common.SystemCommonNs
 
 	u := &mcis.DeploymentPlan{}
 	if err := c.Bind(u); err != nil {
