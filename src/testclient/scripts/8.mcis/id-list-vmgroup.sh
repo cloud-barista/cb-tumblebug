7d759118fb36d5b6a08c0bdf06d24c421ec700a6
Jihoon Seo <jihoon.seo@etri.re.kr>
2022-09-26 11:35:01 +0900

    Update NCP-VPC & NHN Cloud regions & zones

---
+++ src/testclient/scripts/8.mcis/id-list-vmgroup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "####################################################################"
+echo "## 8. VM: List ID"
+echo "####################################################################"
+
+source ../init.sh
+
+if [ "${INDEX}" == "0" ]; then
+	MCISID=${POSTFIX}
+else
+	MCISID=${CONN_CONFIG[$INDEX,$REGION]}-${POSTFIX}
+fi
+
+echo "${MCISID}"
+
+curl -H "${AUTH}" -sX GET http://$TumblebugServer/tumblebug/ns/$NSID/mcis/${MCISID}/vmgroup | jq ''
+
+
+#get_mcis
