4bba5fa1eea3344f43d28f667f88a878465953dd
Seokho Son <shsongist@gmail.com>
2024-01-19 11:58:29 +0900

    Enhance Dockerfile and build

--- scripts/runTumblebug.sh
+++ scripts/runTumblebug.sh
@@ -2,7 +2,7 @@
 
 CONTAINER_NAME_READ="CB-Tumblebug"
 CONTAINER_VERSION="latest"
-CONTAINER_PORT="-p 1323:1323 -p 50252:50252"
+CONTAINER_PORT="-p 1323:1323"
 CONTAINER_DATA_PATH="/app/meta_db"
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
