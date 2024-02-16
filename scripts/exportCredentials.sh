50658b48effbc7a4ca004235161f65acaa16c4e6
Seokho Son <shsongist@gmail.com>
2024-02-16 16:52:50 +0900

    Fix double quote to prevent globbing and word splitting

--- scripts/exportCredentials.sh
+++ scripts/exportCredentials.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -z "$CBTUMBLEBUG_ROOT" ]; then
-    SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]-$0})
-    export CBTUMBLEBUG_ROOT=$(cd $SCRIPT_DIR && cd .. && pwd)
+    SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]-$0}")
+    export CBTUMBLEBUG_ROOT=$(cd "$SCRIPT_DIR" && cd .. && pwd)
 fi
 
 credentialDir="$CBTUMBLEBUG_ROOT/conf"
@@ -101,7 +101,7 @@ cat > "$saveTo/gcp_credential.json" << EOF
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/${client_email//@/%40}",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/${gcp_client_email//@/%40}",
     "universe_domain": "googleapis.com"
 }
 EOF
