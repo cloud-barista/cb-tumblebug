009b8bab1bb35987a5bc84da13e04fb9b6a3bceb
Seokho Son <shsongist@gmail.com>
2024-03-22 17:19:53 +0900

    Generalize fetch region zone tool

--- scripts/misc/update-cloudinfo.py
+++ scripts/misc/update-cloudinfo.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-# This script fetches AWS regions and zones and updates the cloudinfo.yaml file with the latest data.
+# This script fetches csp regions and zones and updates the cloudinfo.yaml file with the latest data.
 # It also compares the updated file with the original file and shows the diff.
 
 # The script uses the following external APIs or CLIs:
 # - Nominatim API: To fetch location details for each region. (https://nominatim.openstreetmap.org/search)
-# - AWS CLI: To fetch AWS regions and zones and region description.
+# - csp CLI: To fetch csp regions and zones and region description.
 
 # The script can be run from the root of the repository using the following command:
 # python3 update-cloudinfo.py
@@ -19,34 +19,48 @@ import yaml
 import json
 import requests
 
-aws_cli_base_command = ['aws']
 nominatim_base_url = 'https://nominatim.openstreetmap.org/search'
 
-# Fetch AWS regions and zones using AWS CLI
-def fetch_aws_regions_and_zones():
-    print("Fetching AWS regions and zones...")
+csp_commands = {
+    'aws': {
+        'regions': ['aws', 'ec2', 'describe-regions', '--all-regions', '--query', 'Regions[*].RegionName', '--output', 'json'],
+        'zones': lambda region: ['aws', 'ec2', 'describe-availability-zones', '--region', region, '--query', 'AvailabilityZones[*].ZoneName', '--output', 'json']
+    },
+    'azure': {
+        'regions': ['az', 'account', 'list-locations', '--query', '[].name', '--output', 'json'],
+        'zones': lambda region: ['az', 'account', 'list-locations', '--query', f'[?name==`{region}`].{region}', '--output', 'json']
+    },
+    # Add more CSPs here
+}
+
+def run_command(command):
+    try:
+        print(f"Running command: {' '.join(command)}")
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        return json.loads(result.stdout), None
+    except subprocess.CalledProcessError as e:
+        print(f"Error running command: {e}")
+        return None, e
+
+# Fetch regions and zones using each CSP CLI
+def fetch_regions_and_zones(csp):
+    regions_command = csp_commands[csp]['regions']
+    print("Fetching regions and zones...")   
     print("(AuthFailure is because of permission issue (Opt-In). You need to opt-in regions)\n")
 
-    try:
-        regions_result = subprocess.run(
-            aws_cli_base_command + ['ec2', 'describe-regions', '--all-regions', '--query', 'Regions[*].RegionName', '--output', 'json'],
-            stdout=subprocess.PIPE, check=True)
-        regions = json.loads(regions_result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Error calling AWS CLI: {e}")
+    regions, error = run_command(regions_command)
+    if error:
         return {}
 
     region_zones = {}
     for region in regions:
         print(f"\n- Fetching availability zones for {region}...")
-        try:
-            zones_result = subprocess.run(
-                aws_cli_base_command + ['ec2', 'describe-availability-zones', '--region', region, '--query', 'AvailabilityZones[*].ZoneName', '--output', 'json'],
-                stdout=subprocess.PIPE, check=True)
-            zones = json.loads(zones_result.stdout)
+        zones_command = csp_commands[csp]['zones'](region)
+        zones, error = run_command(zones_command)
+        if not error:
             print(f"- {zones}")
-        except subprocess.CalledProcessError as e:
-            print(f"Error calling AWS CLI for region {region}: {e}")
+        else:
+            print(f"Failed to fetch zones for region {region}: {error}")
             zones = []
         region_zones[region] = zones
 
@@ -71,11 +85,11 @@ def fetch_location_details(display_name):
     else:
         return {'display': display_name, 'latitude': None, 'longitude': None}
 
-# Fetch region description using AWS CLI
+# Fetch region description using csp CLI
 def fetch_region_description(region_name):
     try:
         result = subprocess.run(
-            aws_cli_base_command + ['ssm', 'get-parameters-by-path', '--path', f'/aws/service/global-infrastructure/regions/{region_name}', '--query', "Parameters[?Name.contains(@,`longName`)].Value", '--output', 'text'],
+            ['aws', 'ssm', 'get-parameters-by-path', '--path', f'/aws/service/global-infrastructure/regions/{region_name}', '--query', "Parameters[?Name.contains(@,`longName`)].Value", '--output', 'text'],
             stdout=subprocess.PIPE, check=True)
         description = result.stdout.decode().strip()
     except subprocess.CalledProcessError as e:
@@ -85,12 +99,12 @@ def fetch_region_description(region_name):
     return description
 
 # Compare and update the cloudinfo.yaml file with the latest data
-def compare_and_update_yaml(cloud_info, output_file_path, current_regions_and_zones):
-    file_aws_regions = set(cloud_info['aws']['region'].keys())
-    current_aws_regions = set(current_regions_and_zones.keys())
+def compare_and_update_yaml(csp, cloud_info, output_file_path, current_regions_and_zones):
+    file_csp_regions = set(cloud_info[csp]['region'].keys())
+    current_csp_regions = set(current_regions_and_zones.keys())
 
-    missing_in_file = current_aws_regions - file_aws_regions
-    extra_in_file = file_aws_regions - current_aws_regions
+    missing_in_file = current_csp_regions - file_csp_regions
+    extra_in_file = file_csp_regions - current_csp_regions
 
     missing_regions_msg = ', '.join(missing_in_file) if missing_in_file else "none"
     extra_regions_msg = ', '.join(extra_in_file) if extra_in_file else "none" 
@@ -99,12 +113,12 @@ def compare_and_update_yaml(cloud_info, output_file_path, current_regions_and_zo
     print(f"- Missing regions: {missing_regions_msg}")
     print(f"- Obsoleted regions: {extra_regions_msg}\n\n") 
 
-    for region in current_aws_regions:
+    for region in current_csp_regions:
         if region in missing_in_file:
             desc = fetch_region_description(region)
             display = desc.split('(')[-1].rstrip(')') if '(' in desc else desc  # Improved parsing
             location_details = fetch_location_details(display)
-            cloud_info['aws']['region'][region] = {
+            cloud_info[csp]['region'][region] = {
                 'desc': desc,
                 'location': location_details,
                 'zone': current_regions_and_zones[region]
@@ -114,7 +128,7 @@ def compare_and_update_yaml(cloud_info, output_file_path, current_regions_and_zo
             print(f"Location: {location_details['display']} ({location_details['latitude']}, {location_details['longitude']})")
             print(f"Zones: {', '.join(current_regions_and_zones[region])}\n")
         else:
-            cloud_info['aws']['region'][region]['zone'] = current_regions_and_zones[region]
+            cloud_info[csp]['region'][region]['zone'] = current_regions_and_zones[region]
             print(f"Updated zones for region: {region}")
             print(f"Zones: {', '.join(current_regions_and_zones[region])}\n")
 
@@ -147,9 +161,9 @@ def main():
         print(f"Error reading file {yaml_file_path}: {e}")
         return
 
-    current_regions_and_zones = fetch_aws_regions_and_zones()
+    current_regions_and_zones = fetch_regions_and_zones('aws')
     
-    compare_and_update_yaml(cloud_info, output_file_path, current_regions_and_zones)
+    compare_and_update_yaml('aws', cloud_info, output_file_path, current_regions_and_zones)
 
     run_git_diff(yaml_file_path, output_file_path)
 
