ae51112646894e0d0a2ce6c860bfce232ac2283b
Seokho Son <shsongist@gmail.com>
2024-06-13 03:51:44 +0900

    Remove echo_message from deployOpenWebUI.sh

--- scripts/usecases/llm/deployOpenWebUI.sh
+++ scripts/usecases/llm/deployOpenWebUI.sh
@@ -9,13 +9,13 @@ else
 fi
 
 # Install Docker
-echo_message "Installing Docker"
+echo "Installing Docker"
 curl -fsSL https://raw.githubusercontent.com/cloud-barista/cb-tumblebug/main/scripts/installDocker.sh | sh
 
 # Run Open WebUI container
-echo_message "Running Open WebUI container"
+echo "Running Open WebUI container"
 sudo docker run -d -p 80:8080 $OLLAMA_BASE_URLS -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 
 # Display the status of the Open WebUI container
-echo_message "Displaying the status of the Open WebUI container"
+echo "Displaying the status of the Open WebUI container"
 sudo docker ps -f name=open-webui
