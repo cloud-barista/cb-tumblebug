139aafef718575b76c1c95d337023093a5db80b7
Sooyoung Kim <sykim.etri@gmail.com>
2024-08-12 18:36:50 +0900

    add testscripts for persistent volume and service in k8scluster

---
+++ src/testclient/scripts/13.k8scluster/get-available-k8scluster-version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "####################################################################"
+echo "## 13. K8SCLUSTER: Get Available Version"
+echo "####################################################################"
+
+source ../init.sh
+
+K8SCLUSTERID_ADD=${OPTION03:-1}
+
+K8SCLUSTERID=${K8SCLUSTERID_PREFIX}${INDEX}${REGION}${K8SCLUSTERID_ADD}
+
+echo "NSID: "${NSID}
+echo "K8SCLUSTERID=${K8SCLUSTERID}"
+
+PROVIDERNAME="alibaba"
+#REGIONNAME="me-east-1"
+REGIONNAME="ap-south-1"
+
+resp=$(
+	curl -H "${AUTH}" -sX GET http://$TumblebugServer/tumblebug/availableK8sClusterVersion?providerName=${PROVIDERNAME}\&regionName=${REGIONNAME}
+	); echo ${resp} | jq ''
+    echo ""
