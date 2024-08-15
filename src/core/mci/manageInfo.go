a8e28e0e4500076ad252a4be3011a32c55ca1e62
seokho-son <shsongist@gmail.com>
2024-08-16 01:46:02 +0900

    Remove infra service terms

--- src/core/mci/manageInfo.go
+++ src/core/mci/manageInfo.go
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package mci is to manage multi-cloud infra service
+// Package mci is to manage multi-cloud infra
 package mci
 
 import (
