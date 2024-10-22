e4e771797a25d3ac87fb350c583bc79a9c997fed
Seokho Son <shsongist@gmail.com>
2024-10-22 23:52:53 +0900

    Enhance large scale mc-infra provisioning stability

--- src/core/infra/control.go
+++ src/core/infra/control.go
@@ -312,7 +312,7 @@ func ControlMciAsync(nsId string, mciId string, action string, force bool) error
 			wg.Add(1)
 
 			// Avoid concurrent requests to CSP.
-			time.Sleep(time.Duration(3) * time.Second)
+			time.Sleep(time.Millisecond * 800)
 
 			go ControlVmAsync(&wg, nsId, mciId, vmId, action, results)
 		}
